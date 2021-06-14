bcb26951644bbbf1d17298764e2774243c6f9d32
Flinner <flinner.hlqn@aleeas.com>
2021-06-14 08:01:26 +0300

    feat: impl Add, Mul<Color,f64>, Sub and PartialEq

--- src/features/colors.rs
+++ src/features/colors.rs
@@ -1,4 +1,9 @@
-//
+use core::f64;
+use std::ops::{Add, Mul, Sub};
+
+#[derive(Clone, Copy, Debug)]
+/// RGB color triplet, values idly be between
+/// /// `0` and `1`, but not restriced.
 pub struct Color {
     pub red: f64,
     pub green: f64,
@@ -6,9 +11,66 @@ pub struct Color {
 }
 
 impl Color {
-    /// Generate a new `Color`. values idly be between
-    /// `0` and `1`, but not restriced.
+    /// Generate a new `Color`.
     pub fn new(red: f64, green: f64, blue: f64) -> Color {
         Color { red, green, blue }
+    }
+}
+
+impl Add for Color {
+    type Output = Self;
+
+    /// Add two `Color`s to each other
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            red: self.red + rhs.red,
+            green: self.green + rhs.green,
+            blue: self.blue + rhs.blue,
+        }
+    }
+}
+
+impl Sub for Color {
+    type Output = Self;
+    /// Subtract two `Color`s from each other
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            red: self.red - rhs.red,
+            green: self.green - rhs.green,
+            blue: self.blue - rhs.blue,
+        }
+    }
+}
+
+impl Mul<Color> for Color {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self {
+            red: self.red * rhs.red,
+            green: self.green * rhs.green,
+            blue: self.blue * rhs.blue,
+        }
+    }
+}
+
+impl Mul<f64> for Color {
+    type Output = Self;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Self {
+            red: self.red * rhs,
+            green: self.green * rhs,
+            blue: self.blue * rhs,
+        }
+    }
+}
+
+impl PartialEq for Color {
+    fn eq(&self, other: &Self) -> bool {
+        use std::f64::EPSILON;
+        (self.red - other.red).abs() < EPSILON
+            && (self.green - other.green).abs() < EPSILON
+            && (self.blue - other.blue).abs() < EPSILON
     }
 }
--- tests/colors.rs
+++ tests/colors.rs
@@ -7,3 +7,38 @@ fn generate_color_with_new() {
     assert_eq!(color.green, 0.4);
     assert_eq!(color.blue, 0.7);
 }
+
+#[test]
+fn add_two_colors() {
+    let color1 = Color::new(0.9, 0.6, 0.75);
+    let color2 = Color::new(0.7, 0.1, 0.25);
+
+    let exepected_color = Color::new(1.6, 0.7, 1.0);
+    assert_eq!(color1 + color2, exepected_color)
+}
+
+#[test]
+fn subtract_two_colors() {
+    let color1 = Color::new(0.9, 0.6, 0.25);
+    let color2 = Color::new(0.7, 0.1, 0.75);
+
+    let exepected_color = Color::new(0.2, 0.5, -0.5);
+    assert_eq!(color1 - color2, exepected_color)
+}
+
+#[test]
+fn multiply_two_colors() {
+    let color1 = Color::new(0.9, 0.6, -1.0);
+    let color2 = Color::new(1.0, 0.1, 0.25);
+
+    let exepected_color = Color::new(0.9, 0.06, -0.25);
+    assert_eq!(color1 * color2, exepected_color)
+}
+
+#[test]
+fn multiply_a_color_by_scalar() {
+    let color1 = Color::new(0.9, -0.6, 0.75);
+
+    let exepected_color = Color::new(1.8, -1.2, 1.5);
+    assert_eq!(color1 * 2.0, exepected_color)
+}
