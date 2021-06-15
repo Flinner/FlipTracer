f12b38e6eaaebd3b380c31b01da367c11ce84459
Flinner <flinner.hlqn@aleeas.com>
2021-06-15 04:51:33 +0300

    doc: add documenation to `Color`

--- src/features/canvas.rs
+++ src/features/canvas.rs
@@ -2,6 +2,7 @@ use super::colors::Color;
 use crate::features::colors;
 
 #[derive(Debug)]
+/// Canvas, a grid of pixels, with `height` and `width` (for faster access)
 pub struct Canvas {
     pub width: usize,
     pub height: usize,
@@ -9,6 +10,7 @@ pub struct Canvas {
 }
 
 impl Canvas {
+    /// Returns a new blank `Canvas` with `colors::BLACK`
     pub fn new(width: usize, height: usize) -> Self {
         Canvas {
             width,
@@ -17,11 +19,12 @@ impl Canvas {
         }
     }
 
+    /// Modify (`x`, `y`) to `color`. Mutates `self`
     pub fn write(self: &mut Self, x: usize, y: usize, color: Color) {
         //(&self.grid[width][height]) = &color;
         self.grid[x][y] = color;
     }
-
+    /// Get `Color` at point (`x`,`y`). Equivalent to `self.grid[x][y]`
     pub fn get(self, x: usize, y: usize) -> Color {
         self.grid[x][y]
     }
