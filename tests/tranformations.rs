13f1046e88bdb6a38b8db7e1fe42bab9add578e3
Flinner <flinner.hlqn@aleeas.com>
2021-06-20 13:52:25 +0300

    test: tranformations * vector

--- tests/tranformations.rs
+++ tests/tranformations.rs
@@ -1,7 +1,4 @@
-use raytracer::math::{
-    point::Point,
-    transformations::{self, Transformation},
-};
+use raytracer::math::{point::Point, transformations::Transformation, vector::Vector};
 
 #[test]
 fn multiplying_point_by_translation_matrix() {
@@ -22,4 +19,12 @@ fn multiplying_point_by_inverse_of_translation_matrix() {
     let expected = Point::new(-8.0, 7.0, 3.0);
 
     assert_eq!(inv.clone().unwrap() * point, expected);
+}
+
+#[test]
+fn multiplying_vector_by_translation_matrix() {
+    let transform = Transformation::translation(5.0, -3.0, 2.0);
+    let vector = Vector::new(-3.0, 4.0, 5.0);
+
+    assert_eq!(transform * vector, vector);
 }
