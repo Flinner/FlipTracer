d633123e691ef7811b95fc9c727fc5f9d57fe5a2
Flinner <flinner.hlqn@aleeas.com>
2021-06-21 19:48:54 +0300

    test: modularize transformations

--- tests/tranformations.rs
+++ tests/tranformations.rs
@@ -1,60 +1,67 @@
 use raytracer::math::{point::Point, transformations::Transformation, vector::Vector};
 
-#[test]
-fn multiplying_point_by_translation_matrix() {
-    let transform = Transformation::translation(5.0, -3.0, 2.0);
-    let point = Point::new(-3.0, 4.0, 5.0);
+mod translation {
+    use super::*;
+    #[test]
+    fn multiply_point() {
+        let transform = Transformation::translation(5.0, -3.0, 2.0);
+        let point = Point::new(-3.0, 4.0, 5.0);
 
-    let expected = Point::new(2.0, 1.0, 7.0);
+        let expected = Point::new(2.0, 1.0, 7.0);
 
-    assert_eq!(transform * point, expected)
+        assert_eq!(transform * point, expected)
+    }
+
+    #[test]
+    fn inverse_multiply_point() {
+        let transform = Transformation::translation(5.0, -3.0, 2.0);
+        let inv = transform.inverse();
+        let point = Point::new(-3.0, 4.0, 5.0);
+
+        let expected = Point::new(-8.0, 7.0, 3.0);
+
+        assert_eq!(inv.clone().unwrap() * point, expected);
+    }
+
+    #[test]
+    fn multiply_vector() {
+        let transform = Transformation::translation(5.0, -3.0, 2.0);
+        let vector = Vector::new(-3.0, 4.0, 5.0);
+
+        assert_eq!(transform * vector, vector);
+    }
 }
 
-#[test]
-fn multiplying_point_by_inverse_of_translation_matrix() {
-    let transform = Transformation::translation(5.0, -3.0, 2.0);
-    let inv = transform.inverse();
-    let point = Point::new(-3.0, 4.0, 5.0);
+mod scaling {
+    use super::*;
 
-    let expected = Point::new(-8.0, 7.0, 3.0);
+    #[test]
+    fn multiply_point() {
+        let transform = Transformation::scaling(2.0, 3.0, 4.0);
+        let point = Point::new(-4.0, 6.0, 8.0);
 
-    assert_eq!(inv.clone().unwrap() * point, expected);
-}
+        let expected = Point::new(-8.0, 18.0, 32.0);
 
-#[test]
-fn multiplying_vector_by_translation_matrix() {
-    let transform = Transformation::translation(5.0, -3.0, 2.0);
-    let vector = Vector::new(-3.0, 4.0, 5.0);
+        assert_eq!(transform * point, expected)
+    }
 
-    assert_eq!(transform * vector, vector);
-}
+    #[test]
+    fn multiply_vector() {
+        let transform = Transformation::scaling(2.0, 3.0, 4.0);
+        let vector = Vector::new(-4.0, 6.0, 8.0);
 
-#[test]
-fn multiplying_point_by_scaling_matrix() {
-    let transform = Transformation::scaling(2.0, 3.0, 4.0);
-    let point = Point::new(-4.0, 6.0, 8.0);
+        let expected = Vector::new(-8.0, 18.0, 32.0);
+        assert_eq!(transform * vector, expected)
+    }
 
-    let expected = Point::new(-8.0, 18.0, 32.0);
+    #[test]
+    fn inverse_multiply_vector() {
+        let transform = Transformation::scaling(2.0, 3.0, 4.0);
+        let inv = transform.inverse();
+        let vector = Vector::new(-4.0, 6.0, 8.0);
 
-    assert_eq!(transform * point, expected)
-}
+        let expected = Vector::new(-2.0, 2.0, 2.0);
 
-#[test]
-fn multiplying_vector_by_scaling_matrix() {
-    let transform = Transformation::scaling(2.0, 3.0, 4.0);
-    let vector = Vector::new(-4.0, 6.0, 8.0);
-
-    let expected = Vector::new(-8.0, 18.0, 32.0);
-    assert_eq!(transform * vector, expected)
-}
-
-#[test]
-fn multiplying_vector_by_inverse_of_scaling_matrix() {
-    let transform = Transformation::scaling(2.0, 3.0, 4.0);
-    let inv = transform.inverse();
-    let vector = Vector::new(-4.0, 6.0, 8.0);
-
-    let expected = Vector::new(-2.0, 2.0, 2.0);
-
-    assert_eq!(inv.unwrap() * vector, expected);
+        assert_eq!(inv.unwrap() * vector, expected);
+    }
 }
