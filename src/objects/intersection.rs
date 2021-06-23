873f6261f016b9e124f0cb81cdb06fd76d0c7603
Flinner <flinner.hlqn@aleeas.com>
2021-06-23 23:51:29 +0300

    feat: Intersection.get

--- src/objects/intersection.rs
+++ src/objects/intersection.rs
@@ -6,4 +6,11 @@ impl Intersection {
     pub fn count(&self) -> usize {
         self.list.len()
     }
+    pub fn get(&self, index: usize) -> Option<f64> {
+        if index > self.list.len() {
+            None
+        } else {
+            Some(self.list[index])
+        }
+    }
 }
