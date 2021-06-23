a5d1fc2e8f988292156259a74c4c7ec17f70b095
Flinner <flinner.hlqn@aleeas.com>
2021-06-23 14:18:41 +0300

    feat: Ray.position

--- src/math/ray.rs
+++ src/math/ray.rs
@@ -11,4 +11,9 @@ impl Ray {
     pub fn new(origin: Point, direction: Vector) -> Ray {
         Ray { origin, direction }
     }
+
+    /// Returns the `Point` in  `distance` from the `origin: Point` in `direction: Vector`
+    pub fn position(&self, distance: f64) -> Point {
+        self.origin + self.direction * distance
+    }
 }
--- tests/ray.rs
+++ tests/ray.rs
@@ -10,3 +10,22 @@ fn create_ray() {
     assert_eq!(ray.origin, origin);
     assert_eq!(ray.direction, direction);
 }
+
+#[test]
+fn position_from_distance() {
+    let origin = Point::new(2.0, 3.0, 4.0);
+    let direction = Vector::new(1.0, 0.0, 0.0);
+
+    let ray = Ray::new(origin, direction);
+
+    let tests = vec![
+        (ray.position(0.0), Point::new(2.0, 3.0, 4.0)),
+        (ray.position(1.0), Point::new(3.0, 3.0, 4.0)),
+        (ray.position(-1.0), Point::new(1.0, 3.0, 4.0)),
+        (ray.position(2.5), Point::new(4.5, 3.0, 4.0)),
+    ];
+
+    for test in tests {
+        assert_eq!(test.0, test.1);
+    }
+}
