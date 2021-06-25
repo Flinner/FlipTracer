464013aa01867c2e87804afe3c4c76f2e07693d7
Flinner <flinner.hlqn@aleeas.com>
2021-06-25 07:00:17 +0300

    feat: Sphere.intersection

--- src/objects/intersection.rs
+++ src/objects/intersection.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 pub struct Intersection {
     pub list: Vec<f64>,
 }
--- src/objects/sphere.rs
+++ src/objects/sphere.rs
@@ -1,6 +1,6 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 
-use crate::math::ray::Ray;
+use crate::math::{point, ray::Ray};
 
 use super::intersection::Intersection;
 
@@ -14,8 +14,23 @@ impl Sphere {
         Self { uid: time_now() }
     }
 
-    pub fn intersects(&self, _ray: Ray) -> Intersection {
-        todo!()
+    pub fn intersects(&self, ray: Ray) -> Option<Intersection> {
+        let sphere_to_ray = ray.origin - point::ORIGIN;
+
+        let a = ray.direction.dot_product(&ray.direction);
+        let b = 2.0 * ray.direction.dot_product(&sphere_to_ray);
+        let c = sphere_to_ray.dot_product(&sphere_to_ray) - 1.0;
+
+        let discriminant = b.powf(2.0) - 4.0 * a * c;
+
+        if discriminant < 0.0 {
+            return None;
+        } else {
+            let t1: f64 = (-b - discriminant.sqrt()) / (2.0_f64 * a);
+            let t2: f64 = (-b + discriminant.sqrt()) / (2.0_f64 * a);
+            let list = vec![t1, t2];
+            return Some(Intersection { list });
+        }
     }
 }
 
